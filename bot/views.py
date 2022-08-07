5c326b3e5425a2a07f393fa556a906417a945e3c
Akash <20bcs020@iiitdmj.ac.in>
2022-08-07 22:00:50 +0530

    whatsAppbot minor changes

--- bot/views.py
+++ bot/views.py
@@ -43,7 +43,7 @@ def sendWhatsAppMessage(name,phoneNo,msg):
     op=webdriver.ChromeOptions()
     op.binary_location=os.environ.get("GOOGLE_CHROME_BIN")  
     # op.add_argument("--headless")
-    # op.add_argument("--no-sandbox")
+    op.add_argument("--no-sandbox")
     op.add_argument("--disable-dev-sh-usage")
     driver=webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"),chrome_options=op)
 
